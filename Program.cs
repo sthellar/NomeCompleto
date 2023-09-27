458c0c6e4839a8b7a28b4b7f744f1242283ba046
Esther Silva <141463785+sthellar@users.noreply.github.com>
2023-09-27 20:45:10 -0300

    Adicinar a solução da tarefa NomeCompleto

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string nome, sobrenome;
+Console.Write("Digite seu nome ");
+nome = Console.ReadLine()!;
+Console.Write("Digite seu último sobrenome ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine("Aperte a proxima tecla para continuar");
+Console.ReadLine();
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome},{nome}");
